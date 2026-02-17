29babef9eda219d4f0aeca1a61720753a8fbf221
Aleksandr Konstantinov <constoff@bk.ru>
2026-02-17 15:59:36 +0500

    Crud test

---
+++ CRUD app/src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,29 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CarServiceImpl implements CarService{
+    private List<Car> carList;
+
+    @Override
+    public List<Car> howMany(int count) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Mersedes-Benz", 5, 2020));
+        cars.add(new Car("Audi", 6, 2018));
+        cars.add(new Car("Gaz", 3, 2022));
+        cars.add(new Car("Lada", 2107, 1995));
+        cars.add(new Car("Toyota", 200, 2015));
+
+        if (count == 0 || count >= 5) {
+            return cars;
+        }
+        return cars.stream().limit(count).collect(Collectors.toList());
+    }
+
+}
